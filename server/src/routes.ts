1e75b5de969eeaa57889af72faddbf08be1d552a
vnsfc <vnsfc@cin.ufpe.br>
2026-04-26 22:59:22 -0300

    complemento routes e controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import {  readAllUsers } from "./controllers/UserController";
+import {  readAllUsers, createCalcado, deleteCalcado, updateCalcado, readAllCalcados } from "./controllers/UserController";
 
 
 const routes = express.Router();
 
 routes.get("/users", readAllUsers);
+routes.post("/calcados", createCalcado);
+routes.delete("/calcados", deleteCalcado);
+routes.patch("/calcados", updateCalcado);
+routes.get("/calcados", readAllCalcados);
 
 
 export default routes;
